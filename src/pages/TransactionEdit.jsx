4102c83588a28b080eddaed54abaa56b639a3965
Oshra Hartuv <oshrartuv@gmail.com>
2022-08-08 17:25:09 +0300

    added click outside

--- src/pages/TransactionEdit.jsx
+++ src/pages/TransactionEdit.jsx
@@ -1,11 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from '../hooks/useForm';
 import { transactionService } from '../services/transactionService';
 import { saveTransaction } from '../store/actions/transactionActions';
+import { useClickOutside } from '../hooks/useClickOutside';
 
-export const TransactionEdit = (props) => {
+export const TransactionEdit = () => {
     const [transaction, handleChange, setTransaction] = useForm(null);
     const { id } = useParams();
     const navigate = useNavigate();
@@ -16,6 +17,10 @@ export const TransactionEdit = (props) => {
     }, [id]);
 
     const dispatch = useDispatch();
+
+    const modalRef = useRef();
+    useClickOutside(modalRef, ()=>{navigate('/')})
+
 
     const loadTransaction = async () => {
         const transaction = id
@@ -41,37 +46,39 @@ export const TransactionEdit = (props) => {
 
     if (!transaction) return <div>Loading...</div>;
     return (
-        <section className="transaction-edit">
-            <h1>{transaction._id ? 'Edit' : 'Add'} Transaction</h1>
-            <form onSubmit={onSaveTransaction}>
-                <label htmlFor="title">Title</label>
-                <input
-                    onChange={handleChange}
-                    value={transaction.title}
-                    type="text"
-                    name="title"
-                    id="title"
-                />
-                <label htmlFor="amount">Amount</label>
-                <input
-                    onChange={handleChange}
-                    value={transaction.amount}
-                    type="number"
-                    name="amount"
-                    id="amount"
-                />
-                <label htmlFor="date">Date</label>
-                <input
-                    onChange={handleChange}
-                    value={new Date(transaction.time)
-                        .toISOString()
-                        .slice(0, 10)}
-                    type="date"
-                    name="time"
-                    id="date"
-                />
-                <button>Save</button>
-            </form>
-        </section>
+        <div className="modal-wrapper">
+            <section className="transaction-edit wrapped-modal" ref={modalRef} >
+                <h1>{transaction._id ? 'Edit' : 'Add'} Transaction</h1>
+                <form onSubmit={onSaveTransaction}>
+                    <label htmlFor="title">Title</label>
+                    <input
+                        onChange={handleChange}
+                        value={transaction.title}
+                        type="text"
+                        name="title"
+                        id="title"
+                    />
+                    <label htmlFor="amount">Amount</label>
+                    <input
+                        onChange={handleChange}
+                        value={transaction.amount}
+                        type="number"
+                        name="amount"
+                        id="amount"
+                    />
+                    <label htmlFor="date">Date</label>
+                    <input
+                        onChange={handleChange}
+                        value={new Date(transaction.time)
+                            .toISOString()
+                            .slice(0, 10)}
+                        type="date"
+                        name="time"
+                        id="date"
+                    />
+                    <button>Save</button>
+                </form>
+            </section>
+        </div>
     );
 };
